751aefd3d7036e2c144ab180bf0109ec808785f2
ianacodev <ianaco.dev@gmail.com>
2018-05-06 01:05:29 -0400

    update seed

---
+++ config/webpack.test.js
@@ -0,0 +1,34 @@
+// core
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  context: __dirname,
+  devtool: 'sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loaders: ['raw-loader', 'sass-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['raw-loader', 'sass-loader']
+      },
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.SourceMapDevToolPlugin({
+      filename: null,
+      test: /\.(ts|js)($|\?)/i
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
